9dd2c19db15fb441cba71306fd716bd6a9594d69
Anthony Hall <anthonyhall@Anthonys-MacBook-Pro.local>
2023-03-25 19:39:26 -0400

    Updated file to include a working navigation bar.

--- music_library/src/App.js
+++ music_library/src/App.js
@@ -1,23 +1,27 @@
+import { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from "./SearchBar";
+import MusicTable from "./MusicTable";
+import NavigationBar from "./NavigationBar";
+import axios from "axios";
 
 function App() {
+  let [music, setMusic] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/songs/")
+      .then((response) => {
+        console.log(response.data)
+        setMusic(response.data);
+      })
+      .then((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar />
     </div>
   );
 }
---
+++ music_library/src/MusicTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MusicTable() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MusicTable
